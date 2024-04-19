d7038ffb0b3ab0386893cc360080971122654fec
Letícia Fariasf 2b <133364942+laliMaryLeticinha231dsnFabisDil@users.noreply.github.com>
2024-04-19 16:57:20 -0300

    hh

---
+++ explicaSexta/explicaSexta.cpp
@@ -0,0 +1,134 @@
+
+// switch é escolha 
+
+#include <iostream>
+#include <locale>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "pt_br.UTF-8");
+	/*int num = 5;
+	switch (num)
+	{
+	case 1:
+		cout << "E o numero 1";
+		break;
+	case 2:
+		cout << "E o numero 2";
+		break;
+	default:
+		cout << "Outro numero";
+		break;
+	}*/
+
+
+	/* char letra;
+	cout << "Digite uma letra: ";
+	cin >> letra;
+	switch (letra)
+	{
+	case 'a':
+		cout << "É a letra a";
+		break;
+	case 'e':
+		cout << "É a letra e";
+		break;
+	case 'j':
+		cout << "É a letra j";
+		break;
+	default:
+		cout << "É outra letra";
+		break;
+	}
+
+	*/
+
+
+	/*int numero;
+	cout << "Digite um numero: ";
+	cin >> numero;
+	switch (numero)
+	{
+	case 1:
+		cout << "Voce e linda";
+		break;
+	case 2:
+		cout << "Feia!";
+		break;
+	case 3:
+		cout << "Tchau";
+		break;
+	default:
+		cout << " Opcao invalida";
+		break;
+
+	}*/
+
+
+
+	/////////////////////////////////EXERCICIOS
+
+	// EXERCICIO 1
+
+	/*int numero;
+	cout << "Digite um numero: ";
+	cin >> numero;
+	switch (numero)
+	{
+	case 1:
+		cout << "Domingo";
+		break;
+	case 2:
+		cout << "Segunda!";
+		break;
+	case 3:
+		cout << "terca";
+		break;
+
+	case 4:
+		cout << "quarta";
+		break;
+	case 5:
+		cout << "quinta";
+		break;
+	case 6:
+		cout << "Sexta";
+		break;
+	case 7:
+		cout << "sabado";
+		break;
+	default:
+		cout << " dia invalido";
+		break;
+
+	}
+} */
+
+
+//EXERCICIO 3
+
+
+	char letra;
+	cout << "Digite uma letra: ";
+	cin >> letra;
+	switch (letra)
+	{
+	case 'a':
+		cout << "VOGAL";
+		break;
+	case 'e':
+		cout << "VOGAL";
+		break;
+	case 'o':
+		cout << "VOGAL";
+		break;
+	case 'u':
+		cout << "VOGAL";
+		break;
+	default:
+		cout << "CONSOANTE";
+		break;
+	}
+
+}
+
